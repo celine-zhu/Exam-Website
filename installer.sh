74074872bc20bd2b01450cc7ae3cf522db4467ca
Flavien Ledeux <Flavien.Ledeux@telecomnancy.eu>
2021-05-05 20:02:58 +0200

    ajout installer version linux

---
+++ installer.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+function LinuxInstallation() {
+    env python3 --version &> /dev/null
+    if [ $? -gt 0 ]; then
+        echo "python non trouvé --> installation de python"
+        echo "detection du package manager:"
+        which apt-get &> /dev/null
+        if [[ $? -eq 0 && -z $_Package ]]; then
+            echo "DPKG package manager found"
+            _Package="apt-get"
+        fi
+        which dnf &> /dev/null
+        if [[ $? -eq 0 && -z $_Package ]]; then
+            echo "Dandified YUM package manager found"
+            _Package="dnf"
+        fi
+        which dnf &> /dev/null
+        if [[ $? -eq 0 && -z $_Package ]]; then
+            echo "Yellowdog Updater Modified package manager found"
+            _Package="yum"
+        fi
+
+        if [[ -z $_Package ]]; then
+            echo "package manager not handled"
+            return 1
+        fi
+
+        eval "$_Package install python3"
+
+
+    fi
+    mkdir ProjectEnvironnement &>/dev/null
+    python3 -m venv ./ProjectEnvironnement
+    ./ProjectEnvironnement/bin/python3 -m pip install --upgrade pip
+    ./ProjectEnvironnement/bin/pip install click
+    ./ProjectEnvironnement/bin/pip install flask
+    return 0
+}
+
+
+function DarwinInstallation() {
+    echo "not implemented yet"
+    return 1
+}
+
+unameOut="$(uname -s)"
+if [[ $unameOut == Darwin ]]; then
+    echo "darwin"
+    DarwinInstallation
+elif [[ $unameOut == Linux ]]; then
+    echo "machien linux"
+    LinuxInstallation
+else
+    echo "machine non reconnue ou nongérer par le script : installation manuel obligatoire"
+fi
+
+
+
+
+
+
