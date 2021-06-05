0d86e54e8191bcc1d531c5b25a22a1002fa94d67
Flavien Ledeux <Flavien.Ledeux@telecomnancy.eu>
2021-06-05 13:06:24 +0200

    ajout de bonus_interclassement dans la bdd et de son implementation pour l'upload

--- src/ParserFichiers.py
+++ src/ParserFichiers.py
@@ -218,11 +218,11 @@ def UploadClasse(liste):
         cur = con.cursor()
         res = cur.execute("SELECT code FROM candidat WHERE code=?",(line[0],)).fetchall()
         if res:
-            query = "UPDATE candidat SET nom=?, prenom=?, code_voie=?, resultat=?, email=?, ad_1=?, cod_pos=?, com=?, pay_adr=?, por=?, date_naissance=?, n_demi=? rang_classe=? WHERE code=?"
-            cur.execute(query, (line[1], line[2], fil, type_admin, line[5], line[6], line[7], commune, country, line[10], line[11], line[12], line[len(line)-1], line[0],))
+            query = "UPDATE candidat SET nom=?, prenom=?, code_voie=?, resultat=?, email=?, ad_1=?, cod_pos=?, com=?, pay_adr=?, por=?, date_naissance=?, n_demi=?, bonus_interclassement=?  rang_classe=? WHERE code=?"
+            cur.execute(query, (line[1], line[2], fil, type_admin, line[5], line[6], line[7], commune, country, line[10], line[11], line[12], line[len(line)-6], line[len(line)-1], line[0],))
         else:
-            query = "INSERT INTO candidat(code, nom, prenom, code_voie, resultat, email, ad_1, cod_pos, com, pay_adr, por, date_naissance, n_demi, rang_classe) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-            cur.execute(query, (line[0], line[1], line[2], fil, type_admin, line[5], line[6], line[7], commune, country, line[10], line[11], line[12], line[len(line)-1],))
+            query = "INSERT INTO candidat(code, nom, prenom, code_voie, resultat, email, ad_1, cod_pos, com, pay_adr, por, date_naissance, n_demi, bonus_interclassement, rang_classe) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+            cur.execute(query, (line[0], line[1], line[2], fil, type_admin, line[5], line[6], line[7], commune, country, line[10], line[11], line[12], line[len(line)-6], line[len(line)-1],))
         j = 0;
         while j < len(ecrit_mat):
             query = "INSERT INTO notes(can_code, matiere_id,type_id, value) VALUES(?,?,?,?)"
